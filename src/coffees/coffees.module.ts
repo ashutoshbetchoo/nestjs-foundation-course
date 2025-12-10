1c68db2a5e75701cd783fbc85d567a588af0a78c
ashutoshbetchoo <64080592+ashutoshbetchoo@users.noreply.github.com>
2025-12-10 23:51:16 +0400

    Dependency Injection

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,10 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffees.entity';
 import { Flavour } from './entities/flavour.entity';
 import { Event } from 'src/events/entities/event.entity/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavour, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: () => ['buddy brew', 'nescafe'],
+    },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
